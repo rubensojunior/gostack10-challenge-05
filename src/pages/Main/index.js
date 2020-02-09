5b837c1ec759dc7dfd20726ee818643a9d75273d
rubensojunior <rubensojunior6@gmail.com>
2020-02-09 11:00:21 -0300

    creating features of repository page

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
 import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
 import api from '../../services/api'
 
-import { Container, Form, SubmitButton, List } from './styles'
+import Container from '../../components/Container'
+import { Form, SubmitButton, List } from './styles'
 
 export default class Main extends Component {
   constructor() {
@@ -81,7 +83,9 @@ export default class Main extends Component {
           {repositories.map(repo => (
             <li key={repo.name}>
               <span>{repo.name}</span>
-              <a href="">Detalhes</a>
+              <Link to={`/repository/${encodeURIComponent(repo.name)}`}>
+                Detalhes
+              </Link>
             </li>
           ))}
         </List>
